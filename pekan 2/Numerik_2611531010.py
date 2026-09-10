1e851d3907ed176275c1dcd20384770b07ae230b
humairazrrt <humairazahratulaini@gmail.com>
2026-09-10 11:15:47 +0700

    first commit

---
+++ pekan 2/Numerik_2611531010.py
@@ -0,0 +1,13 @@
+# Buat file dengan nama Numerik_2611531010.py
+# Nama variabel ditambah 4 digit terakhir NIM contoh: a_1010
+#create a variable with numeric type value.
+a=1010
+print("The type of variable having value",a,"is",type(a))
+
+#create a variable with float value.
+b=10.2345
+print("The type of variable having value",b,"is",type(b))
+
+#create a variable with complex value.
+c=100+3j
+print("The type of variable having value",c,"is",type(c))
