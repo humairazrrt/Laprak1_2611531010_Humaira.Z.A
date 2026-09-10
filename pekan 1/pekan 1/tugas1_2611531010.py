f5c010ca2c2b0d27fcf5655362fe88a1e66a9ad5
humairazrrt <humairazahratulaini@gmail.com>
2026-09-10 23:28:09 +0700

    Add files via upload

--- pekan 1/pekan 1/tugas1_2611531010.py
+++ pekan 1/pekan 1/tugas1_2611531010.py
@@ -1,10 +1,16 @@
-# Menampilkan bentuk hati dengan pola matematika
-for row in range(6):
-    for col in range(7):
-        if (row == 0 and col % 3 != 0) or (row == 1 and col % 3 == 0) or (row - col == 2) or (row + col == 8):
-            print("\033[91m*\033[0m", end=" ") # \033[91m memberi warna merah
-        else:
-            print(" ", end=" ")
-    print()
+# tugas1_2611531010.py
 
-print("\n\033[92m  FOR YOU! <3 \033[0m")
+print("       **       **       ")
+print("     ******   ******     ")
+print("   ********* *********   ")
+print("  *********************  ")
+print(" ************************ ")
+print(" ************************ ")
+print("  *********************  ")
+print("   *******************   ")
+print("    *****************    ")
+print("      *************      ")
+print("        *********        ")
+print("          *****          ")
+print("           ***           ")
+print("            *            ")
